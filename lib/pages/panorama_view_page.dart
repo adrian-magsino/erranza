378e0049345d6b358f91fe12dc9ae646c93ebd85
javarice_plusplus <adrianmagsino2004@gmail.com>
2024-10-21 23:46:45 +0800

    message: Added hotspots message: Added the function of changing scenes when clicking the hotspot

--- lib/pages/panorama_view_page.dart
+++ lib/pages/panorama_view_page.dart
@@ -1,3 +1,4 @@
+
 import 'package:erranza/widgets/hotspot_button.dart';
 import 'package:flutter/material.dart';
 import 'package:panorama_viewer/panorama_viewer.dart';
@@ -11,23 +12,60 @@ class PanoramaViewPage extends StatefulWidget {
 }
 
 class _PanoramaViewState extends State<PanoramaViewPage> {
-
   
-  
+  final List<String> image_list = ['assets/images/360img.jpeg', 'assets/images/360img2.jpeg'];
+  int image_index = 0;
   @override
   Widget build(BuildContext context) {
-    Widget panorama = PanoramaViewer(
-          child: Image.asset('assets/images/360img.jpeg'),
+    Widget panorama;
+    switch(image_index){
+      case 0:
+          panorama = PanoramaViewer(
+          child: Image.asset(image_list[image_index]),
           hotspots: [
             Hotspot(
               latitude: -15.0,
               longitude: -129.0,
               width: 300,
               height: 300,
-              widget: hotspotButton(icon: Icons.open_in_browser, onPressed: () => ()),
+              widget: hotspotButton(icon: Icons.open_in_browser, onPressed: () => {
+                setState(() => image_index = 1),
+              }),
             ),
           ],
           );
+      case 1:
+          panorama = PanoramaViewer(
+          child: Image.asset(image_list[image_index]),
+          hotspots: [
+            Hotspot(
+              latitude: -15.0,
+              longitude: -129.0,
+              width: 300,
+              height: 300,
+              widget: hotspotButton(icon: Icons.open_in_browser, onPressed: () => {
+                setState(() => image_index = 0),
+              }),
+            ),
+          ],
+          );
+      default:
+          panorama = PanoramaViewer(
+          child: Image.asset(image_list[image_index]),
+          hotspots: [
+            Hotspot(
+              latitude: -15.0,
+              longitude: -129.0,
+              width: 300,
+              height: 300,
+              widget: hotspotButton(icon: Icons.open_in_browser, onPressed: () => {
+                setState(() => image_index = 1),
+              }),
+            ),
+          ],
+          );
+    }
+    
 
     return Scaffold(
       appBar: AppBar(
