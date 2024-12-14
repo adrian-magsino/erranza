3e4a12f00c77f0fa3c60c12e9eb66417f861e0a8
javarice_plusplus <adrianmagsino2004@gmail.com>
2024-12-14 19:11:08 +0800

    feedback website will now be accessed directly by clicking feedback button, removed feedback screen

--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -5,6 +5,5 @@ export 'package:erranza/pages/settings_page.dart';
 export 'package:erranza/pages/building_detail_page.dart';
 export 'package:erranza/pages/panorama_view_page.dart';
 export 'package:erranza/pages/tutorial_page.dart';
-export 'package:erranza/pages/feedback_page.dart';
 export 'package:erranza/pages/updates_page.dart';
 export 'package:erranza/pages/about_app_page.dart';
